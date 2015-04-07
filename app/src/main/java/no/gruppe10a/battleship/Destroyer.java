99b033b2e0a4f712b60f8e681b8d83c6693b53f3
Eirik Folkestad <eirik.ek.folkestad@gmail.comcd>
2015-04-07 15:11:09 +0200

    rompe

---
+++ app/src/main/java/no/gruppe10a/battleship/Destroyer.java
@@ -0,0 +1,8 @@
+package no.gruppe10a.battleship;
+
+/**
+ * Created by Eirik on 07/04/15.
+ */
+public class Destroyer extends Ship {
+
+}
---
+++ app/src/main/java/no/gruppe10a/battleship/Ship.java
@@ -0,0 +1,17 @@
+package no.gruppe10a.battleship;
+
+import android.media.Image;
+
+/**
+ * Created by Eirik on 07/04/15.
+ */
+public abstract class Ship {
+    private String name;
+    private int size;
+    private Image img;
+
+    public Ship(String name, int size, Image img) {
+        
+    }
+
+}

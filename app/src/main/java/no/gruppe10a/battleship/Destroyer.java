f19f1d03e15a6dbe1c125b0a109b8fcb172e91bd
perchrib <peberg.91@gmail.com>
2015-04-08 11:19:43 +0200

    new

--- app/src/main/java/no/gruppe10a/battleship/Destroyer.java
+++ app/src/main/java/no/gruppe10a/battleship/Destroyer.java
@@ -1,8 +1,13 @@
 package no.gruppe10a.battleship;
 
+import android.media.Image;
+
 /**
  * Created by Eirik on 07/04/15.
  */
 public class Destroyer extends Ship {
 
+    public Destroyer(String name, int size, Image img) {
+        super(name, size, img);
+    }
 }

8ddc2998b13de295675c398c66c8371b7269ded8
Eirik Folkestad <eirik.ek.folkestad@gmail.comcd>
2015-04-08 15:38:06 +0200

    Added ship classes

---
+++ app/src/main/java/no/gruppe10a/battleship/Battleship.java
@@ -0,0 +1,15 @@
+package no.gruppe10a.battleship;
+
+/**
+ * Created by Eirik on 08/04/15.
+ */
+public class Battleship extends Ship {
+
+
+    public Battleship() {
+        super.setName("Battleship");
+        super.setSize(4);
+        super.setImg(null);
+    }
+
+}

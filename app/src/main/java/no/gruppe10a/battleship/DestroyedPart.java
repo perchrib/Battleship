b0d2d787b8f19d1bc0856e579dd4a82ec6ab1b78
Eirik Folkestad <eirik.ek.folkestad@gmail.comcd>
2015-04-09 13:42:28 +0200

    started on gameboard and made a detroyedship class

---
+++ app/src/main/java/no/gruppe10a/battleship/DestroyedPart.java
@@ -0,0 +1,14 @@
+package no.gruppe10a.battleship;
+
+/**
+ * Created by Eirik on 09/04/15.
+ */
+public class DestroyedPart extends Ship {
+
+    public DestroyedPart() {
+        super.setName("DestroyedPart");
+        super.setSize(1);
+        super.setImg(null);
+        super.setHp(0);
+    }
+}

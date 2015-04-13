cbcfaa5c82f31f61f4f86bdfdfce64e9c7ccdd73
Eirik Folkestad <eirik.ek.folkestad@gmail.comcd>
2015-04-13 12:02:40 +0200

    added classes

---
+++ app/src/main/java/no/gruppe10a/battleship/GameBoard.java
@@ -0,0 +1,86 @@
+package no.gruppe10a.battleship;
+
+import android.media.Image;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Eirik on 09/04/15.
+ */
+public class GameBoard {
+    Ship [][] board;
+    ArrayList<Ship> ships;
+    ArrayList<Ship> destroyedShips;
+
+    public GameBoard() {
+        board = new Ship [10][10];
+        ships = new ArrayList<Ship>();
+        destroyedShips = new ArrayList<Ship>();
+        for (int i = 0; i < board.length; i ++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = null;
+            }
+        }
+    }
+
+    // player adds ships to board and cant add more than the limit
+    public boolean addShip(int [][] pos) {
+
+        if (ships.size() < 5) {
+            switch (pos.length) {
+                case 2:
+                    Destroyer destroyer = new Destroyer();
+                    ships.add(destroyer);
+                    for (int i = 0; i < pos.length; i++) {
+                        board[i][i] = destroyer;
+                    }
+                    return true;
+                case 3:
+                    Submarine submarine = new Submarine();
+                    ships.add(submarine);
+                    for (int i = 0; i < pos.length; i ++) {
+                        board[i][i] = submarine;
+                    }
+                    return true;
+                case 4:
+                    Battleship battleship = new Battleship();
+                    ships.add(battleship);
+                    for (int i = 0; i < pos.length; i++) {
+                        board[i][i] = battleship;
+                    }
+                    return true;
+                case 5:
+                    Carrier carrier = new Carrier();
+                    ships.add(carrier);
+                    for (int i = 0; i < pos.length; i++) {
+                        board[i][i] = carrier;
+                    }
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    // returns true if a part of a ship is hit and false if a DestroyedPart is hit or water is hit
+    public int ShipHit(int [] pos) {
+        if (board[pos[0]][pos[1]] == null) {
+            return 0;
+        } else if (board[pos[0]][pos[1]] instanceof DestroyedPart) {
+                return 1;
+        } else {
+            board[pos[0]][pos[1]].TakeDamage();
+            if (board[pos[0]][pos[1]].isDestroyed()) {
+                ships.remove(board[pos[0]][pos[1]]);
+                destroyedShips.add(board[pos[0]][pos[1]]);
+            }
+            board[pos[0]][pos[1]] = new DestroyedPart();
+            return 2;
+        }
+    }
+
+    public boolean lostGame() {
+        return ships.isEmpty();
+    }
+
+
+}
--- app/src/main/java/no/gruppe10a/battleship/Ship.java
+++ app/src/main/java/no/gruppe10a/battleship/Ship.java
@@ -9,9 +9,32 @@ public abstract class Ship {
     private String name;
     private int size;
     private Image img;
+    private int hp;
 
-    public Ship(String name, int size, Image img) {
-        
+    public void setName(String name) {
+        this.name = name;
     }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setImg(Image img) {
+        this.img = img;
+    }
+
+    public void setHp(int hp) { this.hp = hp; }
+
+    public void TakeDamage() {
+        hp = hp-1;
+    }
+
+    public boolean isDestroyed() {
+        if (hp <= 0) {
+            return true;
+        }
+        return false;
+    }
+
 
 }
